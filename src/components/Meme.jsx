2982b18d5cfb7ec3369c92401e795e9e5fc7eab4
JustInTimez <just.oost@gmail.com>
2023-05-25 13:35:47 +0200

    Adds data file of memes
    So that I can start trying to fetch images

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,14 +1,27 @@
 import React from "react"
+import memeData from "../memeData"
+
 
 export default function Meme() {
+  function fetchMemeImage() {
+    const memesArray = memeData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+    console.log(url)
+  }
+
   return (
     <main className="meme">
         <div className="input-area">
             <input className="input-box" placeholder="Shup up" type="text" />
             <input className="input-box" placeholder="And take my money!" type="text" />
-            <button className="fetch-image-button">🖼️Get a new meme image!🖼️</button>
+            <button 
+                className="fetch-image-button"
+                onClick={fetchMemeImage}
+            >
+                🖼️Get a new meme image!🖼️
+            </button>
         </div>
-
     </main>
   )
 }
