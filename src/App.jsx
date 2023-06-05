6fbb36d1a91fd51c79fc49ef252bfc9043ad5178
JustInTimez <just.oost@gmail.com>
2023-06-05 11:50:04 +0200

    Adds Meme History component
    So that there is some text content on the page as well, giving us a nice synopsis of the Meme world

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
 import Header from './components/Header'
 import Meme from './components/Meme'
+import MemeHistory from './components/MemeHistory'
 import Footer from './components/Footer'
 import './App.css'
 
@@ -10,6 +10,7 @@ function App() {
     <>
       <Header />
       <Meme />
+      <MemeHistory />
       <Footer />
     </>
   )
